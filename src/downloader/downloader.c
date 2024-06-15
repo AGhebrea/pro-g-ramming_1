90a9fb2df8bfd071c40f85f07f977b926f604a35
AGhebrea <alex.ghebrea4@gmail.com>
2024-06-15 09:14:27 +0300

    malloc size fix

--- src/downloader/downloader.c
+++ src/downloader/downloader.c
@@ -7,10 +7,18 @@ const char* TARGET_DIR = "~/Downloads/";
 int downloader_main()
 {
     char* url = "https://artix.unixpeople.org/iso/artix-base-dinit-20230814-x86_64.iso";
+    char* wget_options = "--continue --quiet";
+    char* fmt = "wget %s %s";
 
-    char* cmd = malloc(sizeof(char) * 100 + 1);
-    strcat(cmd, "wget --continue --quiet ");
-    strcat(cmd, url);
+    char *cmd = NULL;
+    size_t n = snprintf(NULL, 0, fmt, wget_options, url);
+    
+    cmd = malloc(sizeof(char) * (n + 1));
+
+    snprintf(cmd, n+1, fmt, wget_options, url);
+    // todo: error handling
+
+    printf("cmd: [%s]", cmd);
 
     system(cmd);
 
